10123c9bb169b1192af5b9bb5c90f022c92f1326
Robin Steiger <alu0101282602@ull.edu.es>
2018-11-20 23:27:15 +0000

    uploading function for satisfying test

--- lib/prct06/human.rb
+++ lib/prct06/human.rb
@@ -12,18 +12,18 @@ class Human
 	def name_to_s
 		"Name: #{@name}\n"
 	end
-	
+
 	def surname_to_s
 		"Surname: #{@surname}\n"
-        end
-	
+	end
+
 	def sex_to_s
 		"Sex: #{@sex}\n"
-        end
-	
+	end
+
 	def age_to_s
 		"Age: #{@age}\n"
-        end
+	end
 end
 
 
@@ -31,7 +31,7 @@ end
 class Anthropometric < Human
 	#getter and setter all in one
 	#attr_accessor :name, :surname, :sex, :age, :height, :weight
-=begin
+
 	def initialize(name, surname, sex, age, height, weight, waist, hip, triceps, bicipital, subscapular, suprailiac, arm)
 		super(name, surname, sex, age)
 		@height, @weight, @waist, @hip, @triceps, @bicipital, @subscapular, @suprailiac, @arm = height, weight, waist, hip, triceps, bicipital, subscapular, suprailiac, arm
@@ -39,39 +39,39 @@ class Anthropometric < Human
 
 	def height_to_s
 		"Height: #{@height}\n"
-        end
-	
+	end
+
 	def weight_to_s
 		"Weight: #{@weight}\n"
-        end
+	end
 
 	def waist_to_s
 		return uni_med(@waist)
-        end
-	
+	end
+
 	def hip_to_s
 		return uni_med(@hip)
 	end
-	
+
 	def triceps_to_s
 		return uni_med(@triceps)
-        end
-	
+	end
+
 	def biceps_to_s
 		return uni_med(@bicipital)
-        end
-	
+	end
+
 	def subscapular_to_s
 		return uni_med(@subscapular)
-        end
-	
+	end
+
 	def suprailiac_to_s
 		return uni_med(@suprailiac)
-        end
+	end
 
 	def arm_to_s
 		return uni_med(@arm)
-        end
+	end
 
 	def uni_med(li)
 		sum, i = 0, 0
@@ -81,19 +81,17 @@ class Anthropometric < Human
 		end
 		return (sum/li.length).round(2)
 	end
-	
+
 	def imc
 		return (@weight/@height**2).round(2)
 	end
-	
+
 
 	def rcc
-	    return (waist_to_s()/hip_to_s).round(2)
+		return (waist_to_s()/hip_to_s).round(2)
 	end
-	
+
 	def porc_fat
 		return (1.2 * imc + 0.23 * @age - 10.8 * @sex -5.4).round(2)
 	end
 end
-=end
-
